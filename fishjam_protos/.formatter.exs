ff2af3345f6b5ccb1c22834f579c3a7043c5081c
Karol Konkol <konkolisz@gmail.com>
2024-11-08 14:58:44 +0100

    Requested changes

--- fishjam_protos/.formatter.exs
+++ fishjam_protos/.formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [field: 1, field: 2, field: 3, field: 4, oneof: 1, oneof: 2]
+  import_deps: [:protobuf]
 ]
