24d0a06ef80f10b5f8bfd5da121375161d4822a2
Karol Konkol <konkolisz@gmail.com>
2024-11-08 11:33:44 +0100

    Check if fishjam_protos are up to date

--- fishjam_protos/.formatter.exs
+++ fishjam_protos/.formatter.exs
@@ -1,4 +1,5 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [field: 1, field: 2, field: 3, field: 4, oneof: 1, oneof: 2]
 ]
--- fishjam_protos/lib/fishjam/peer_notifications.pb.ex
+++ fishjam_protos/lib/fishjam/peer_notifications.pb.ex
@@ -1,56 +1,62 @@
 defmodule Fishjam.PeerMessage.Authenticated do
   @moduledoc false
 
-  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 end
 
 defmodule Fishjam.PeerMessage.AuthRequest do
   @moduledoc false
 
-  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 
-  field(:token, 1, type: :string)
-  field(:sdk_version, 2, type: :string, json_name: "sdkVersion")
+  field :token, 1, type: :string
+  field :sdk_version, 2, type: :string, json_name: "sdkVersion"
 end
 
 defmodule Fishjam.PeerMessage.RTCStatsReport do
   @moduledoc false
 
-  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 
-  field(:data, 1, type: :string)
+  field :data, 1, type: :string
+end
+
+defmodule Fishjam.PeerMessage.MediaEvent do
+  @moduledoc false
+
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
+
+  field :data, 1, type: :string
 end
 
 defmodule Fishjam.PeerMessage do
   @moduledoc false
 
-  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.12.0"
+  use Protobuf, syntax: :proto3, protoc_gen_elixir_version: "0.13.0"
 
-  oneof(:content, 0)
+  oneof :content, 0
 
-  field(:authenticated, 1, type: Fishjam.PeerMessage.Authenticated, oneof: 0)
+  field :authenticated, 1, type: Fishjam.PeerMessage.Authenticated, oneof: 0
 
-  field(:auth_request, 2,
+  field :auth_request, 2,
     type: Fishjam.PeerMessage.AuthRequest,
     json_name: "authRequest",
     oneof: 0
-  )
 
-  field(:peer_media_event, 3,
-    type: Fishjam.MediaEvents.Peer.MediaEvent,
-    json_name: "peerMediaEvent",
-    oneof: 0
-  )
+  field :media_event, 3, type: Fishjam.PeerMessage.MediaEvent, json_name: "mediaEvent", oneof: 0
 
-  field(:server_media_event, 4,
-    type: Fishjam.MediaEvents.Server.MediaEvent,
-    json_name: "serverMediaEvent",
-    oneof: 0
-  )
-
-  field(:rtc_stats_report, 5,
+  field :rtc_stats_report, 4,
     type: Fishjam.PeerMessage.RTCStatsReport,
     json_name: "rtcStatsReport",
     oneof: 0
-  )
+
+  field :peer_media_event, 5,
+    type: Fishjam.MediaEvents.Peer.MediaEvent,
+    json_name: "peerMediaEvent",
+    oneof: 0
+
+  field :server_media_event, 6,
+    type: Fishjam.MediaEvents.Server.MediaEvent,
+    json_name: "serverMediaEvent",
+    oneof: 0
 end
