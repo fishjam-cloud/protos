3d2998c5d9335f8daa56bd70edceddecbdaac3ab
Karol Konkol <56369082+Karolk99@users.noreply.github.com>
2025-03-17 12:21:25 +0100

    Initial release of fishjam protos (#19)

--- fishjam_protos/mix.exs
+++ fishjam_protos/mix.exs
@@ -8,7 +8,19 @@ defmodule FishjamProtos.MixProject do
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      description: "Protobuf modules for Fishjam and Membrane RTC Engine"
+      description: "Protobuf modules for Fishjam and Membrane RTC Engine",
+      package: package()
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Fishjam Team"],
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/fishjam-cloud/protos/tree/master/fishjam_protos",
+        "Fishjam Homepage" => "https://fishjam.io"
+      }
     ]
   end
 
